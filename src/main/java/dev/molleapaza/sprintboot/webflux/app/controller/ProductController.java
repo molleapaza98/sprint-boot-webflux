b7eb3ee28e5c0089ecf98a84c3fa9f1760797a9b
Erick Molleapaza <molleapaza98@gmail.com>
2023-10-21 11:18:27 -0500

    Reactive Data Driven mode to manage back pressure

--- src/main/java/dev/molleapaza/sprintboot/webflux/app/controller/ProductController.java
+++ src/main/java/dev/molleapaza/sprintboot/webflux/app/controller/ProductController.java
@@ -8,7 +8,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @Controller
 public class ProductController {
@@ -33,6 +36,22 @@ public class ProductController {
         products.subscribe(prod -> log.info(prod.getName()));
 
         model.addAttribute("products",products);
+        model.addAttribute("title", "list product");
+        return "list";
+    }
+
+    @GetMapping("/list-data-driver")
+    public String listDataDriver(Model model){
+
+        Flux<Product> products = productDao.findAll()
+                .map(product -> {
+                    product.setName(product.getName().toUpperCase());
+                    return product;
+                }).delayElements(Duration.ofSeconds(1));
+
+        products.subscribe(prod -> log.info(prod.getName()));
+
+        model.addAttribute("products",new ReactiveDataDriverContextVariable(products, 2));
         model.addAttribute("title", "list product");
         return "list";
     }
